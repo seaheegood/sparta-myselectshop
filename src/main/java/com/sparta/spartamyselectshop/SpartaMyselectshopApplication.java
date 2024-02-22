60eda3893f89fa1fba6767ac6ad83fd3d6cf6ca7
Sehee Hong <seaheehong@gmail.com>
2024-02-22 17:49:10 +0900

    first init

---
+++ src/main/java/com/sparta/spartamyselectshop/SpartaMyselectshopApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartamyselectshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaMyselectshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaMyselectshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spartamyselectshop/SpartaMyselectshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartamyselectshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaMyselectshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
